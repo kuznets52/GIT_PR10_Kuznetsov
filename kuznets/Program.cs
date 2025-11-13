7a7c0cbbedaf14f4e428147ebb2464151de35ed4
Vitalya <vitalya@yandex.ru>
2025-11-13 09:36:16 +0300

    Добавил изменение в Program.cs

--- kuznets/Program.cs
+++ kuznets/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine($"Current date: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+         Console.WriteLine("Кузнецов Егор Группа ИС-23Б");
     }
 }
